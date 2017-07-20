40d0288db1338a6ceb5a3f7a53b9aad86dfc6859
Arun Kumar <arun4webtech@gmail.com>
2017-07-20 17:50:19 +0530

    Update insertion_sort.js

--- insertion_sort.js
+++ insertion_sort.js
@@ -1,7 +1,7 @@
 Array.prototype.insert = function(index,item){this.splice(index,0,item);}
 Array.prototype.delete = function(index){this.splice(index,1);}
 Array.prototype.insertSort = function(){
-	for(let i=1;i<=this.length;i++)
+	for(let i=1;i<this.length;i++)
 		for(let j=0;j<i;j++)
 			if(this[i] < this[j])
 			{
